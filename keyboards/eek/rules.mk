b489a83a059d08aa814525d3fc244387d074f0ed
Klackygears <37446827+Klackygears@users.noreply.github.com>
2020-05-14 21:52:52 -0400

    working on flip config

--- keyboards/eek/rules.mk
+++ keyboards/eek/rules.mk
@@ -11,6 +11,7 @@ MCU = atmega32u4
 #   ATmega328P   USBasp
 BOOTLOADER = qmk-dfu
 
+KEYBOARD_LOCAL_FEATURES_MK := $(dir $(lastword $(MAKEFILE_LIST)))local_features.mk
 # Build Options
 #   change yes to no to disable
 #
@@ -31,3 +32,4 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs
+#FLIPPED = 1
