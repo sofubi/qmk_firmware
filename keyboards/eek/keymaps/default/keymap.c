3f9d0241690df8ee75bea6258a1b61eded503022
Klackygears <37446827+Klackygears@users.noreply.github.com>
2020-05-29 00:13:18 -0400

    updates

--- keyboards/eek/keymaps/default/keymap.c
+++ keyboards/eek/keymaps/default/keymap.c
@@ -41,18 +41,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `--------------------'           `--------------------'
  *
  */
- /*  [_BASE] = LAYOUT(
+  [_BASE] = LAYOUT(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
                       KC_LCTL, LOWER, KC_SPC,         KC_BSPC, RAISE, OSM(MOD_LSFT)                 \
-  ), */
-   [_BASE] = LAYOUT(
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
-    KC_A,    KC_S,    RGB_HUD,    RGB_HUI,    RGB_M_P,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
-    KC_Z,    KC_X,    RGB_VAD,    RGB_VAI,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-                      RGB_SAD, RGB_SAI, RGB_MOD,         KC_BSPC, RAISE, OSM(MOD_LSFT)                 \
-  ),
+),
 
   [_LOWER] = LAYOUT(
     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, \
