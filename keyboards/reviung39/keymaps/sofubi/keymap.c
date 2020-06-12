2b2bbb536e2a01f3a87f63cd35af6414a6bba0ff
Thomas Lawton <thomaslawton91@gmail.com>
2020-06-12 05:07:10 -0400

    Updates personal board to add winkey to top level

--- keyboards/reviung39/keymaps/sofubi/keymap.c
+++ keyboards/reviung39/keymaps/sofubi/keymap.c
@@ -45,10 +45,10 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_reviung39(
-    KC_TAB,          KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,            KC_Y,  KC_U,  KC_I,           KC_O,           KC_P,             KC_BSPC,
-    LCTL_T(KC_ESC),  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,            KC_H,  KC_J,  KC_K,           KC_L,           TD(TD_SCLN_COL),  TD(TD_SQUO_DQUO),
-    KC_LSFT,         KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,            KC_N,  KC_M,  TD(TD_DOT_GT),  TD(TD_DOT_GT),  TD(TD_SLSH_QUES), RSFT_T(KC_ENT),
-                                                 LOWER,  KC_SPC,  RAISE
+    KC_TAB,          KC_Q,         KC_W,             KC_E,            KC_R,  KC_T,            KC_Y,  KC_U,  KC_I,           KC_O,           KC_P,             KC_BSPC,
+    LCTL_T(KC_ESC),  KC_A,         KC_S,             KC_D,            KC_F,  KC_G,            KC_H,  KC_J,  KC_K,           KC_L,           TD(TD_SCLN_COL),  TD(TD_SQUO_DQUO),
+    KC_LSFT,         LCTL_T(KC_Z), LGUI_T(KC_X),    LALT_T(KC_C),    KC_V,  KC_B,            KC_N,  KC_M,  TD(TD_DOT_GT),  TD(TD_DOT_GT),  TD(TD_SLSH_QUES), RSFT_T(KC_ENT),
+                                                     LOWER,  KC_SPC,  RAISE
   ),
 
   [_LOWER] = LAYOUT_reviung39(
