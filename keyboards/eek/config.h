2a0a1ffa6d79c4955a3f7105c76cdcc83bd6b5e6
Klackygears <37446827+Klackygears@users.noreply.github.com>
2020-07-01 16:28:59 -0400

    updating matrix for rev 1.3

--- keyboards/eek/config.h
+++ keyboards/eek/config.h
@@ -33,15 +33,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define DIODE_DIRECTION COL2ROW
 
-#define MATRIX_ROW_PINS { E6, B4, B5, B6 }
+//#define MATRIX_ROW_PINS { E6, B4, B5, B6 }
+// above is v1.2, below is v1.3
+#define MATRIX_ROW_PINS { D7, E6, B4, B5 }
 #if FLIPPED
-   #define MATRIX_COL_PINS { F4, F5, F6, F7, B1, D7, C6, D4, D0, D1 }
+   //#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, D7, C6, D4, D0, D1 }
    // above is v1.2, below is v1.3
-   //#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6, C6, D4 }
+   #define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6, C6, D4 }
 #else
-   #define MATRIX_COL_PINS { D1, D0, D4, C6, D7, B1, F7, F6, F5, F4 }
+   // #define MATRIX_COL_PINS { D1, D0, D4, C6, D7, B1, F7, F6, F5, F4 }
    // above is v1.2, below is v1.3
-   //#define MATRIX_COL_PINS { D4, C6, B6, B2, B3, B1, F7, F6, F5, F4 }
+   #define MATRIX_COL_PINS { D4, C6, B6, B2, B3, B1, F7, F6, F5, F4 }
 #endif
 
 #define UNUSED_PINS
@@ -51,7 +53,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define BACKLIGHT_BREATHING
 // #define BACKLIGHT_LEVELS 3
 
- #define RGB_DI_PIN B3
+ //#define RGB_DI_PIN B3
+ // above is v1.2, below is v1.3
+ #define RGB_DI_PIN D3
 #ifdef RGB_DI_PIN
   #define RGBLED_NUM 18
   #define RGBLIGHT_HUE_STEP 4
