4ed47c1a68a7e0abc0623947312b12eef42a5713
Thomas Lawton <thomas@makeros.com>
2020-07-12 21:18:51 -0400

    Updates to reviung, adds corne

--- keyboards/reviung39/keymaps/sofubi/keymap.c
+++ keyboards/reviung39/keymaps/sofubi/keymap.c
@@ -46,7 +46,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_reviung39(
     LGUI_T(KC_TAB),          KC_Q,         KC_W,             KC_E,            KC_R,  KC_T,            KC_Y,  KC_U,  KC_I,           KC_O,           KC_P,             KC_BSPC,
-    LCTL_T(KC_ESC),  KC_A,         KC_S,             KC_D,            KC_F,  KC_G,            KC_H,  KC_J,  KC_K,           KC_L,           TD(TD_SCLN_COL),  TD(TD_SQUO_DQUO),
+    LCTL_T(KC_ESC),  KC_A,         KC_S,             KC_D,            KC_F,  KC_G,            KC_H,  KC_J,  KC_K,           KC_L,           TD(TD_SCLN_COL),  LALT_T(KC_QUOT),
     LSFT_T(KC_ENT),         KC_Z, KC_X,    KC_C,    KC_V,  KC_B,            KC_N,  KC_M,  TD(TD_COMM_LT),  TD(TD_DOT_GT),  TD(TD_SLSH_QUES), RSFT_T(KC_ENT),
                                                      LOWER,  KC_SPC,  RAISE
   ),
