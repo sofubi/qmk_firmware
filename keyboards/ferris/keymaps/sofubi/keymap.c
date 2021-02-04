bd1666bf390d523715935da1a56b3188fc52e9af
Thomas Lawton <thomaslawton91@gmail.com>
2021-02-03 21:26:46 -0500

    mac-updates

--- keyboards/ferris/keymaps/sofubi/keymap.c
+++ keyboards/ferris/keymaps/sofubi/keymap.c
@@ -10,7 +10,7 @@
     K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, \
     K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, \
     K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, \
-    KC_NO, KC_BSPC, LT(7, KC_SPC), KC_NO \
+    LALT(KC_LSFT), KC_BSPC, LT(7, KC_SPC), LCA(KC_LSFT) \
   )
 #define LAYOUT_base_wrapper(...)       LAYOUT_base(__VA_ARGS__)
 
